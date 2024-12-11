4144af8da97655e634d7285bf7cf8e6075117790
niomictomi <niomic.tomi@gmail.com>
2024-12-11 16:02:47 +0700

    feature - add pejabat

---
+++ resources/views/landingpage/landingpejabat.blade.php
@@ -0,0 +1,73 @@
+<div class="container mx-auto py-12 mb-36 md:min-h-screen" id="anggota" >
+    <!-- Pimpinan -->
+    <div class="text-center mb-12">
+        <h1 class="font-bold  text-2xl md:text-5xl text-blue-900 text-center mb-36">
+            Penasihat Khusus Presiden <br /> Bidang Politik dan Keamanan
+        </h1>
+
+    </div>
+
+    <div class="flex flex-col items-center mb-12 border p-6">
+        <h2 class="font-bold mb-4">Penasihat Khusus Presiden Bidang Politik dan Keamanan</h2>
+
+        <img src="{{ asset('img/organisasi/penasehatpng.png') }}" alt="Pimpinan"
+            class="w-24 h-24 rounded-full border shadow mb-4 object-cover">
+        <p class="font-bold">Jenderal TNI (Purn.) H. Wiranto, S.H., S.IP., M.M</p>
+
+    </div>
+
+    <!-- Dua Bidang -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
+        <!-- Asisten I -->
+        <div class="flex flex-col items-center border p-6">
+            <h2 class="font-bold mb-4">
+                Asisten I Bidang Politik</h2>
+            <img src="{{ asset('img/organisasi/asisten1.png') }}" alt="Asisten I"
+                class="w-24 h-24 rounded-full border shadow mb-4 object-cover">
+            <p class="font-bold">DR. H. Adi Warman, S.H., M.H., MBA</p>
+        </div>
+
+        <!-- Asisten II -->
+        <div class="flex flex-col items-center border p-6">
+            <h2 class="font-bold mb-4">Asisten II Bidang Keamanan</h2>
+            <img src="{{ asset('img/organisasi/asisten2.png') }}" alt="Asisten II"
+                class="w-24 h-24 rounded-full border shadow mb-4 object-cover">
+            <p class="font-bold">Letjen TNI (Purn.) Yudhie Swastono</p>
+        </div>
+    </div>
+
+    <!-- Anggota PBAS -->
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <!-- PBAS I -->
+        <div class="flex flex-col items-center border p-4">
+            <h3 class="font-bold mb-2">PBAS I</h3>
+            <img src="{{ asset('img/organisasi/pbas1.jpg') }}" alt="PBAS I"
+                class="w-24 h-24 rounded-full border shadow mb-2 object-cover">
+            <p class="font-bold">Teten Indra Abdillah</p>
+        </div>
+
+        <!-- PBAS II -->
+        <div class="flex flex-col items-center border p-4">
+            <h3 class="font-bold mb-2">PBAS II</h3>
+            <img src="{{ asset('img/organisasi/pbas2.png') }}" alt="PBAS II"
+                class="w-24 h-24 rounded-full border shadow mb-2 object-cover">
+            <p class="font-bold">Kol TNI (Purn) Dr. Alimuchalif Suryo, S.IP., M.A</p>
+        </div>
+
+        <!-- PBAS III -->
+        <div class="flex flex-col items-center border p-4">
+            <h3 class="font-bold mb-2">PBAS III</h3>
+            <img src="{{ asset('img/organisasi/pbas3.jpg') }}" alt="PBAS III"
+                class="w-24 h-24 rounded-full border shadow mb-2 object-cover">
+            <p class="font-bold">Brigjen Pol (Purn) Drs. Erwin Chahara</p>
+        </div>
+
+        <!-- PBAS IV -->
+        <div class="flex flex-col items-center border p-4">
+            <h3 class="font-bold mb-2">PBAS IV</h3>
+            <img src="{{ asset('img/organisasi/pbas4.png') }}" alt="PBAS IV"
+                class="w-24 h-24 rounded-full border shadow mb-2 object-cover">
+            <p class="font-bold">Indra Adi Putra, S.ST., M.M</p>
+        </div>
+    </div>
+</div>
